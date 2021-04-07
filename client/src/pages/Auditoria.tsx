18d40a10226e7b5c320edc1113528623731857ed
jstaricco10 <jstaricco@correo.um.edu.uy>
2021-04-07 19:08:46 -0300

    testing ionic components

--- client/src/pages/Auditoria.tsx
+++ client/src/pages/Auditoria.tsx
@@ -1,10 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSlides, IonSlide } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSlides, IonSlide, IonItem, IonLabel, IonRange, IonItemDivider,IonIcon, IonList, IonSelect, IonSelectOption} from '@ionic/react';
+import { RangeValue } from '@ionic/core';
+import { sunny } from 'ionicons/icons';
+import './Auditoria.css';
 
 const Auditoria: React.FC = () => {
     const slideOpts = {
         initialSlide: 1,
         speed: 400
       };
+    
+    const [value, setValue] = useState(0);
+    const [rangeValue, setRangeValue] = useState<{
+    lower: number;
+    upper: number;
+    }>({ lower: 0, upper: 0 });
+
+    const test = (value: string): void => {
+        console.log(value);
+    }
+
+    const [hairColor, setHairColor] = useState<string>('brown');
 
     return (
         <IonPage>
@@ -19,15 +35,33 @@ const Auditoria: React.FC = () => {
 						<IonTitle size='large'>Nueva Auditoría</IonTitle>
 					</IonToolbar>
 				</IonHeader>
-                <IonSlides pager={true} options={slideOpts} style={{height: '100%'}}>
+                <IonSlides className="slider" pager={true} options={slideOpts} style={{height: '100%'}}>
                     <IonSlide>
                         <h1>Slide 1</h1>
                     </IonSlide>
                     <IonSlide>
-                        <h1>Slide 2</h1>
+                        <IonItem onFocus={() => test("si")} style={{width: '100%'}}>
+                            <IonRange min={0} max={100} color="secondary" pin={true} value={0} disabled={false} onIonChange={e => setValue(e.detail.value as number)} >
+                                <IonLabel slot="start">0</IonLabel>
+                                <IonLabel slot="end">100</IonLabel>
+                            </IonRange>
+                        </IonItem>
                     </IonSlide>
                     <IonSlide>
-                        <h1>Slide 3</h1>
+                        <IonList>
+                            <IonLabel>Hair Color</IonLabel>
+                            <IonSelect value={hairColor} okText="Okay" cancelText="Dismiss" onIonChange={e => setHairColor(e.detail.value)}>
+                                <IonSelectOption value="brown">Brown</IonSelectOption>
+                                <IonSelectOption value="blonde">Blonde</IonSelectOption>
+                                <IonSelectOption value="black">Black</IonSelectOption>
+                                <IonSelectOption value="red">Red</IonSelectOption>
+                            </IonSelect>                
+                            <IonItemDivider>Your Selections</IonItemDivider>
+                            <IonItem>Hair Color: {hairColor}</IonItem>
+                        </IonList>
+                    </IonSlide>
+                    <IonSlide>
+                        <h1>Slide 4</h1>
                     </IonSlide>
                 </IonSlides>
             </IonContent>
