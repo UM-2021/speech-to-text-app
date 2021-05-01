baa98adcc3eecf94a1fb8d60c2a2b5293b67002b
Joaco <jnfernandez.ojeda@gmail.com>
2021-05-01 09:57:27 -0300

    first commit github actions

--- server/api/tests.py
+++ server/api/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+# python manage.py test app_articles.tests.tests_users.ExampleTestCase
+class ExampleTestCase(APITestCase):
+
+    def test_example(self):
+        user_data = {
+            "username": "Pablo",
+            "email": "Pablo@g.com",
+            "gender": "M",
+            "birth": "2000-12-12T06:55:00Z",
+            "level": "SR",
+            "password": "Pablo"
+        }
+        response = self.client.post('/users/', user_data, format='json')
+
+        self.assertEqual(response.status_code, 201)
