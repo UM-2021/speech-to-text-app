a98da4c8352d39b9ead7dd4613c71c70fca6bba9
Jose Algorta <jpalgorta@correo.um.edu.uy>
2021-06-05 17:45:35 -0300

    (IDSII-84): (WIP) Changes in the backend to match desired state.

--- server/sucursal/serializers.py
+++ server/sucursal/serializers.py
@@ -3,6 +3,7 @@ from api.models import Sucursal
 
 
 class SucursalSerializer(serializers.ModelSerializer):
+    ultimo_responsable = serializers.ReadOnlyField(source='ultimo_responsable.username')
     class Meta:
         model = Sucursal
         fields = '__all__'
