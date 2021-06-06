9f21145351ba816ff07cd3d8d45b3858d62416dd
Jose Algorta <jpalgorta@correo.um.edu.uy>
2021-06-05 21:43:12 -0300

    (IDSII-95): Add labels to questions.

--- client/src/components/Pregunta.tsx
+++ client/src/components/Pregunta.tsx
@@ -1,3 +1,4 @@
+import { IonChip } from '@ionic/react';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ADD_RESPUESTA } from '../actions/types';
@@ -13,6 +14,7 @@ interface IPregunta {
   pregunta: string;
   tipo: string;
   opciones?: any;
+  categoria: string;
 }
 
 const Pregunta: React.FC<IPregunta> = ({
@@ -21,6 +23,7 @@ const Pregunta: React.FC<IPregunta> = ({
   pregunta,
   opciones,
   auditoriaId,
+  categoria
 }) => {
   const dispatch = useDispatch();
 
@@ -32,31 +35,37 @@ const Pregunta: React.FC<IPregunta> = ({
     });
   }, [dispatch, id, auditoriaId]);
 
+  const colors: any = {
+    DIGEFE: 'danger',
+    Informativa: 'success',
+    Extranormativa: 'warning'
+  }
+  
   return (
-    <div className="ion-padding flex ion-margin-vertical">
-      <div>
-        <h3>{pregunta}</h3>
-      </div>
-      <div>
-        <h5>
-          <i>Respuesta: </i>
-        </h5>
-        <div>
-          {respuestas.filter(
-            (r: any) => r.pregunta === parseInt(id) && r.isAnswered
-          )[0]?.respuesta || ''}
-        </div>
-      </div>
+		<div className='ion-padding flex ion-margin-vertical'>
+			<div className="flex-int">
+				<IonChip className='ion-align-self-start' outline color={colors[categoria]}>
+					{categoria}
+				</IonChip>
+				<h3>{pregunta}</h3>
+			</div>
+			<div>
+				<h5>
+					<i>Respuesta: </i>
+				</h5>
+				<div>
+					{respuestas.filter((r: any) => r.pregunta === parseInt(id) && r.isAnswered)[0]
+						?.respuesta || ''}
+				</div>
+			</div>
 
-      <div className="shrink">
-        {tipo === 'audi' && <div></div>}
-        {tipo === 'opci' && (
-          <PreguntaOpciones opciones={opciones} preguntaId={id} />
-        )}
-        {tipo === 'nume' && <PreguntaNumerica preguntaId={id} />}
-        <PreguntaAudio preguntaId={id} />
-      </div>
-    </div>
+			<div className='shrink'>
+				{tipo === 'audi' && <div></div>}
+				{tipo === 'opci' && <PreguntaOpciones opciones={opciones} preguntaId={id} />}
+				{tipo === 'nume' && <PreguntaNumerica preguntaId={id} />}
+				<PreguntaAudio preguntaId={id} />
+			</div>
+		</div>
   );
 };
 
