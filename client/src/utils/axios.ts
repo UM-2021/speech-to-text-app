15d81bffac29c2fe5d2297dbaec2cfa7d0abf390
Jose Algorta <jpalgorta@correo.um.edu.uy>
2021-06-07 18:22:12 -0300

    (IDSII-86): Set HTTPS URL.

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import { isPlatform } from '@ionic/react';
 import axios from 'axios';
 
-const BASE_URL = isPlatform('capacitor') ? 'http://testpagos.tk' : 'http://localhost:8000';
+const BASE_URL = isPlatform('capacitor') ? 'https://testpagos.tk' : 'http://localhost:8000';
 
 const axiosInstance = axios.create({
 	baseURL: BASE_URL
