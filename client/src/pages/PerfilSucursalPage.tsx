ad955c4a8a50d3a18bb32458aa55a4013514c645
Jose Algorta <jpalgorta@correo.um.edu.uy>
2021-06-11 01:44:37 -0300

    (IDSII-102): Banner to indicate the status of an Auditoria.

--- client/src/pages/PerfilSucursalPage.tsx
+++ client/src/pages/PerfilSucursalPage.tsx
@@ -1,21 +1,23 @@
-import {
-	IonButton,
-	IonButtons,
-	IonContent,
-	IonHeader,
-	IonIcon,
-	IonPage,
-	IonTitle,
-	IonToolbar
-} from '@ionic/react';
+import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonTitle, IonToolbar } from '@ionic/react';
 import { arrowBack } from 'ionicons/icons';
 import React from 'react';
 import { Link, RouteComponentProps, useHistory } from 'react-router-dom';
+import Message from '../components/Message';
 import PageWrapper from '../components/PageWrapper';
 import PerfilSucursal from '../components/PerfilSucursal';
 
+interface Auditoria {
+	finalizada?: boolean;
+	aprobada?: boolean;
+}
+
 const PerfilSucursalPage: React.FC<RouteComponentProps<{ id: string }>> = ({ match }) => {
 	let history = useHistory();
+	// For testing
+	const auditoria: Auditoria = {
+		finalizada: true,
+		aprobada: false
+	};
 
 	return (
 		<PageWrapper>
@@ -43,6 +45,16 @@ const PerfilSucursalPage: React.FC<RouteComponentProps<{ id: string }>> = ({ mat
 						</IonButtons>
 					</IonToolbar>
 				</IonHeader>
+				{Object.keys(auditoria).length === 0 ? (
+					<Message color='light'>Local no auditado</Message>
+				) : auditoria.aprobada ? (
+					<Message color='success'>Auditoria finalizada: Local habilitado</Message>
+				) : auditoria.finalizada && !auditoria.aprobada ? (
+					<Message color='danger'>Auditoria finalizada: Local no habilitado</Message>
+				) : !auditoria.finalizada ? (
+					<Message color='primary'>Auditoria en curso</Message>
+				) : null}
+
 				<PerfilSucursal id={match.params.id} />
 			</IonContent>
 		</PageWrapper>
