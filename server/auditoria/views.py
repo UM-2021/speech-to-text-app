200f3b81eaffe22c7bb5ada716b2046085a57f3b
IgnacioCogliatti <jcogliatti@correo.um.edu.uy>
2021-06-21 15:36:44 -0300

    precisa juanma

--- server/auditoria/views.py
+++ server/auditoria/views.py
@@ -245,7 +245,7 @@ class IncidenteViewSet(viewsets.ModelViewSet):
     serializer_class = IncidenteSerializer
 
     def list(self, request):
-        queryset_incidente = Incidente.objects.filter(Q(reporta=request.user.id) | Q(asignado=request.user.id))
+        queryset_incidente = Incidente.objects.filter(Q(reporta=request.user.id), ~Q(estado='Confirmado') | Q(asignado=request.user.id), ~Q(estado='Pendiente'))
         incidente_serializer = IncidenteSerializer(queryset_incidente, many=True)
         # Añadir el nombre de la sucursal
         result = []
